07c6ad5a42cc6a501519814bad1ad7b1d6f3b2d5
ZafarHanif <bestowzafar@gmail.com>
2024-05-01 01:30:29 +0500

    Enhanced UI with color-coded outputs

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
+import chalk from "chalk";
 import inquirer from "inquirer";
-console.log("Wellcome to CodeWithZefi - CLI Number Guessing Game");
-// 1. Computer Will generate a random number - DONE
-// 2. User input for guessing number - DONE
-// 3. Compare user input with computer generated number and show result - DONE
+console.log(chalk.yellow.bold("Welcome to ") +
+    chalk.cyan.bold("CodeWithZefi - CLI Number Guessing Game\n"));
 const randomNumber = Math.floor(Math.random() * 5 + 1);
 const answers = await inquirer.prompt([
     {
         name: "userGuessNumber",
         type: "number",
-        message: "Please guess a number between 1-5:",
+        message: chalk.blue.bold("Please guess a number between 1-5:"),
     },
 ]);
 if (answers.userGuessNumber === randomNumber) {
-    console.log("Congratulations! You guessed the right number ");
+    console.log(chalk.green.bold("\nCongratulations! You guessed the right number 🎉\n"));
 }
 else {
-    console.log("Sorry, you guessed the wrong number.");
+    console.log(chalk.red.bold("\nSorry, you guessed the wrong number 😔 Better luck next time!\n"));
 }
